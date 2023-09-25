11eaf34c6aec85c5f31b823618e50f045560659b
Laila Nabil <laila.n.mostafa@gmail.com>
2023-09-25 09:48:07 +0300

    continue project setup

---
+++ lib/common/widgets/responsive.dart
@@ -0,0 +1,82 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+enum ResponsiveDevice {
+  mobile(400),
+  tablet(800),
+  startShowingSmallDesign(1100),
+  laptop(1440);
+
+  final int value;
+
+  const ResponsiveDevice(this.value);
+}
+
+class ResponsiveTParams<T> {
+  final T mobile;
+  final T? tablet;
+  final T? startShowingSmallDesign;
+  final T laptop;
+
+  ResponsiveTParams(
+      {required this.mobile,
+      this.tablet,
+      this.startShowingSmallDesign,
+      required this.laptop});
+}
+
+class Responsive {
+  static bool showSmallDesign(BuildContext context) =>
+      MediaQuery.sizeOf(context).width <=
+      ResponsiveDevice.startShowingSmallDesign.value;
+
+  static T responsiveT<T>({
+    required ResponsiveTParams params,
+    required BuildContext context,
+  }) {
+    final device = getDevice(context);
+    switch (device) {
+      case (ResponsiveDevice.mobile):
+        return params.mobile;
+      case (ResponsiveDevice.tablet):
+        return params.tablet ?? params.mobile;
+      case (ResponsiveDevice.startShowingSmallDesign):
+        return params.startShowingSmallDesign ?? params.tablet ?? params.mobile;
+      case (ResponsiveDevice.laptop):
+        return params.laptop;
+    }
+  }
+
+  static ResponsiveDevice getDevice(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    if (width >= ResponsiveDevice.laptop.value) {
+      return ResponsiveDevice.laptop;
+    }
+    if (width >= ResponsiveDevice.startShowingSmallDesign.value) {
+      return ResponsiveDevice.startShowingSmallDesign;
+    }
+    if (width >= ResponsiveDevice.tablet.value) {
+      return ResponsiveDevice.tablet;
+    }
+    return ResponsiveDevice.mobile;
+  }
+}
+
+class ResponsiveScaffold extends Scaffold {
+  final BuildContext context;
+
+  ///[responsiveBody] overrides [body]
+  final ResponsiveTParams<Widget> responsiveBody;
+
+  const ResponsiveScaffold(
+      {required this.responsiveBody, required this.context, super.key});
+
+  @override
+  Widget? get body {
+    return Responsive.responsiveT(params: responsiveBody, context: context);
+  }
+
+  @override
+  // ignore: recursive_getters
+  Widget? get drawer => Responsive.showSmallDesign(context) ? drawer : null;
+}
--- lib/core/network/network.dart
+++ lib/core/network/network.dart
@@ -1,11 +1,17 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 abstract class Network {
   final Client httpClient;
 
-  Future<NetworkResponse> post();
+  Future<NetworkResponse> post(
+      {required Uri url,
+      Map<String, String>? headers,
+      Object? body,
+      Encoding? encoding});
 
-  Future<NetworkResponse> get();
+  Future<NetworkResponse> get({required Uri url, Map<String, String>? headers});
 
   Network({required this.httpClient});
 }
