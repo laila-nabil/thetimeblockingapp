73c2000e201a11d3374150b1253f08603b0ae439
Laila Nabil <laila.n.mostafa@gmail.com>
2023-09-25 10:40:00 +0300

    continue project setup

---
+++ lib/core/extensions.dart
@@ -0,0 +1,17 @@
+extension ElementAtNullableOrEmpty<T> on List<T>?{
+  T? get tryFirst{
+    if(this!=null && this?.isNotEmpty == true){
+      return this?.first;
+    }else{
+      return null;
+    }
+  }
+
+  T?  tryElementAt(int index){
+    if(this!=null && this?.isNotEmpty == true && (index < (this?.length ??0) == true)){
+      return this?.elementAt(index);
+    }else{
+      return null;
+    }
+  }
+}
