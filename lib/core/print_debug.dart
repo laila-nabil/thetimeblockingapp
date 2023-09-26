b326530aa448123a798584f59487a10e62172458
Laila Nabil <laila.n.mostafa@gmail.com>
2023-09-26 15:06:53 +0300

    auth page setup #11 - .
    TODO: auth page => explain then redirect => kdebug true ? input code : take code from url then save it to global then locally

--- lib/core/print_debug.dart
+++ lib/core/print_debug.dart
@@ -10,7 +10,7 @@ void printDebug(Object? object, {PrintLevel? printLevel = PrintLevel.trace}) {
   }
 }
 
-LogPrinter logPrinter = PrettyPrinter(noBoxingByDefault: true, methodCount: 10);
+LogPrinter logPrinter = PrettyPrinter(noBoxingByDefault: true, methodCount: 0);
 
 void _printLogger(PrintLevel? printLevel, Object? object) {
   final logger = serviceLocator<Logger>();
