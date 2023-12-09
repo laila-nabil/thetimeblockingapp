b849793f4a5ff4362a3f3e4bc4b5108629c29ec9
Laila Nabil <laila.nabil.mustafa@gmail.com>
2023-12-09 15:34:10 +0200

    test

--- test/common/entities/clickup_user_test.dart
+++ test/common/entities/clickup_user_test.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:thetimeblockingapp/common/entities/clickup_user.dart';
-import 'dart:developer';
 import 'package:timezone/data/latest_all.dart'
     if (kIsWeb) 'core/mock_web_packages/mock_timezone.dart' as tz_not_web;
 import 'package:timezone/timezone.dart';
@@ -11,7 +12,7 @@ void main() {
   });
   test('clickup user getTimezone', () {
     const user =  ClickupUser(timezone: "Africa/Cairo");
-    log("user ${user.getTimezone}");
+    print("user ${user.getTimezone}");
     expect(user.getTimezone,
         const TimeZone(7200000, isDst: false, abbreviation: "EET"));
   });
