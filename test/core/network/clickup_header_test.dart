a5a3d14da741aac9120f3c686b1b8c1846dfffb1
Laila Nabil <laila.nabil.mustafa@gmail.com>
2023-12-09 16:14:21 +0200

    test

--- test/core/network/clickup_header_test.dart
+++ test/core/network/clickup_header_test.dart
@@ -1,7 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:thetimeblockingapp/core/network/clickup_header.dart';
+import 'package:thetimeblockingapp/features/auth/domain/entities/clickup_access_token.dart';
 
 void main() {
-  test('clickup header ...', () {
-    ///TODO TEST
+  test('clickup header test', () {
+    const clickupAccessToken = "abcToken";
+    expect(
+        clickupHeader(
+            clickupAccessToken:
+                const ClickupAccessToken(accessToken: clickupAccessToken,tokenType: "Bearer")),
+        {
+          "Authorization": clickupAccessToken,
+          "Content-Type": "application/json"
+        });
   });
 }
