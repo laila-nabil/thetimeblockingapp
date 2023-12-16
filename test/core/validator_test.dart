dff307ccff20d7e1c67e2500fb6d198c8d84c94f
Laila Nabil <laila.nabil.mustafa@gmail.com>
2023-12-16 14:27:22 +0200

    test

--- test/core/validator_test.dart
+++ test/core/validator_test.dart
@@ -1,7 +1,36 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:thetimeblockingapp/core/validator.dart';
 
 void main() {
-  test('validator ...', () {
-    ///TODO TEST
+  group('validator tests', () {
+    group('isEmailValid tests', () {
+      test('isEmailValid for "laila@gmail.com" should return true', () {
+        expect(Validator.isEmailValid("laila@gmail.com"), true);
+      });
+      test('isEmailValid for "@gmail.com" should return false" ', () {
+        expect(Validator.isEmailValid("@gmail.com"), false);
+      });
+      test('isEmailValid for "_@gmail.com" should return false" ', () {
+        expect(Validator.isEmailValid("_@gmail.com"), false);
+      });
+      test('isEmailValid for "1@gmail.com" should return false" ', () {
+        expect(Validator.isEmailValid("1@gmail.com"), false);
+      });
+      test('isEmailValid for "gmail.com" should return false" ', () {
+        expect(Validator.isEmailValid("gmail.com"), false);
+      });
+      test('isEmailValid for "" should return false" ', () {
+        expect(Validator.isEmailValid(""), false);
+      });
+      test('isEmailValid for " " should return false" ', () {
+        expect(Validator.isEmailValid(" "), false);
+      });
+      test('isEmailValid for "@.com" should return false" ', () {
+        expect(Validator.isEmailValid("@.com"), false);
+      });
+      test('isEmailValid for "laila@asu.edu" should return true" ', () {
+        expect(Validator.isEmailValid("laila@asu.edu"), true);
+      });
+    });
   });
 }
