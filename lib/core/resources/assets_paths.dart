37e326114c92feec6078d23b7ba68b20b11e45ca
Laila Nabil <laila.nabil.mustafa@gmail.com>
2024-02-02 20:13:32 +0200

    custom drawer

--- lib/core/resources/assets_paths.dart
+++ lib/core/resources/assets_paths.dart
@@ -5,4 +5,17 @@ class AppAssets {
   static const multiplyCirclePng = "assets/images/multiply_circle.png";
   static const multiplyCircleSvg = "assets/images/multiply_circle.svg";
   static const dotsVPng = "assets/images/dots_v.png";
+  static const calendar = "assets/images/calendar@2x.png";
+  static const bin = "assets/images/bin@2x.png";
+  static const chevronRight = "assets/images/chevron-right@2x.png";
+  static const chevronDown = "assets/images/chevron-down@2x.png";
+  static const folder = "assets/images/folder@2x.png";
+  static const github = "assets/images/github@2x.png";
+  static const twitter = "assets/images/twitter@2x.png";
+  static const hashtag = "assets/images/hashtag@2x.png";
+  static const list = "assets/images/list@2x.png";
+  static const infoCircle = "assets/images/info-circle@2x.png";
+  static const map = "assets/images/map-alt@2x.png";
+  static const settings = "assets/images/settings@2x.png";
+  static const logo = "assets/images/logo@2x.png";
 }
