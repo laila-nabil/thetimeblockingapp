df0b201d6617e56a75302baaa308cc9be7deacd1
Laila Nabil <laila.nabil.mustafa@gmail.com>
2024-07-21 23:36:41 +0300

    move

--- lib/common/enums/backend_mode.dart
+++ lib/common/enums/backend_mode.dart
@@ -12,37 +12,4 @@ enum BackendMode {
       this == BackendMode.offlineWithCalendarSync;
 }
 
-class ClickupGlobals {
-  final String clickupUrl;
 
-  final String clickupClientId;
-
-  final String clickupClientSecret;
-
-  final String clickupRedirectUrl;
-
-  final String clickupTerms = "https://clickup.com/terms";
-  final String clickupPrivacy = "https://clickup.com/terms/privacy";
-  String get clickupAuthUrl =>
-      "https://app.clickup.com/api?client_id=$clickupClientId&redirect_uri=$clickupRedirectUrl";
-
-  ClickupGlobals(
-      {this.clickupUrl = "",
-      this.clickupClientId = "",
-      this.clickupClientSecret = "",
-      this.clickupRedirectUrl = ""});
-
-  ClickupGlobals copyWith({
-    String? clickupUrl,
-    String? clickupClientId,
-    String? clickupClientSecret,
-    String? clickupRedirectUrl,
-  }) {
-    return ClickupGlobals(
-      clickupUrl: clickupUrl ?? this.clickupUrl,
-      clickupClientId: clickupClientId ?? this.clickupClientId,
-      clickupClientSecret: clickupClientSecret ?? this.clickupClientSecret,
-      clickupRedirectUrl: clickupRedirectUrl ?? this.clickupRedirectUrl,
-    );
-  }
-}
